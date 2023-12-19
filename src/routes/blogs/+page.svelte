97422c1424af0fb04128288d06d97f4d6d826b0c
Adrian Montoya <montoya@astralta.com>
2023-12-19 11:53:27 +0100

    Initial commit

---
+++ src/routes/blogs/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<section class="w-full overflow-auto flex pt-20 flex-col items-center h-full min-h-screen">
+    
+</section>
---
+++ src/routes/blogs/[blogId]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<section class="w-full overflow-auto flex pt-20 flex-col items-center h-full min-h-screen">
+    <div class="w-full justify-center flex items-center flex-col h-full py-10 pt-20">
+        Your blog is yet to come!
+
+    </div>
+
+</section>
